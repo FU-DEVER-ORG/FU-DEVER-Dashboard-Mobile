69a6da34b78475bbdc38651862e7d01c9ec02c15
hulaun <vangolahung4@gmail.com>
2024-05-02 17:26:59 +0700

    add skills page UI

--- lib/modules/screens/members/skills.dart
+++ lib/modules/screens/members/skills.dart
@@ -10,52 +10,8 @@ class Skills extends StatefulWidget {
 }
 
 class _SkillsState extends State<Skills> {
-  final _formKey = GlobalKey<FormState>();
-  late bool obscureCurrentPassword;
-  late bool obscureNewPassword;
-  late bool obscureVerifiedPassword;
 
-  String currentPassword = '';
-  String newPassword = '';
-  String v_password = '';
-
-  void handleSubmission() async {
-    if (_formKey.currentState!.validate()) {
-      //signUp(originalPassword, password);
-      Navigator.of(context).pushNamed('/');
-    }
-  }
-
-  Future<void> signUp(String originalPassword, String password) async {
-    final url = 'http://api/Auth/sign-up';
-
-    final response = await http.post(
-      Uri.parse(url),
-      body: jsonEncode({
-        'originalPassword': originalPassword,
-        'password': password,
-      }),
-      headers: {'Content-Type': 'application/json'},
-    );
-
-    if (response.statusCode == 200) {
-      // Successful login
-      final responseData = jsonDecode(response.body);
-      print(responseData);
-    } else {
-      // Error handling
-      print('Login failed. Status code: ${response.statusCode}');
-    }
-  }
-
-  @override
-  void initState() {
-    obscureCurrentPassword = true;
-    obscureNewPassword = true;
-    obscureVerifiedPassword = true;
-    super.initState();
-  }
-
+  List<String> skills = ["Java", "Figma", "Flutter"];
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
@@ -63,188 +19,25 @@ class _SkillsState extends State<Skills> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Đổi mật khẩu",
+          "Kỹ năng",
           style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
         ),
         backgroundColor: Theme.of(context).colorScheme.background,
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text("Mật khẩu hiện tại"),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: obscureCurrentPassword,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: '*********',
-                    suffixIcon: IconButton(
-                      icon: const Icon(
-                        Icons.visibility,
-                        color: Colors.grey, // Customize the eye icon color
-                      ),
-                      onPressed: () {
-                        // Toggle the obscureText value when the eye icon is pressed
-                        setState(() {
-                          obscureCurrentPassword = !obscureCurrentPassword;
-                        });
-                      },
-                    ),
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      currentPassword = value!;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Enter a valid password'; // Changed error message
-                    } else if (!RegExp(r'^[\w]+$').hasMatch(value)) {
-                      return 'Enter a valid password'; // Changed error message
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
-                const Text("Mật khẩu mới"),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: obscureNewPassword,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Nhập mật khẩu mới',
-                    suffixIcon: IconButton(
-                      icon: const Icon(
-                        Icons.visibility,
-                        color: Colors.grey, // Customize the eye icon color
-                      ),
-                      onPressed: () {
-                        // Toggle the obscureText value when the eye icon is pressed
-                        setState(() {
-                          obscureNewPassword = !obscureNewPassword;
-                        });
-                      },
-                    ),
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      newPassword = value!;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Enter a valid password'; // Changed error message
-                    } else if (!RegExp(r'^[\w]+$').hasMatch(value)) {
-                      return 'Enter a valid password'; // Changed error message
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
-                const Text("Xác nhận"),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: obscureVerifiedPassword,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.red, width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Nhập lại mật khẩu mới',
-                    suffixIcon: IconButton(
-                      icon: const Icon(
-                        Icons.visibility,
-                        color: Colors.grey, // Customize the eye icon color
-                      ),
-                      onPressed: () {
-                        // Toggle the obscureText value when the eye icon is pressed
-                        setState(() {
-                          obscureVerifiedPassword = !obscureVerifiedPassword;
-                        });
-                      },
-                    ),
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      v_password = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Enter a valid password'; // Changed error message
-                    } else if (!RegExp(r'^[\w]+$').hasMatch(value)) {
-                      return 'Enter a valid password'; // Changed error message
-                    } else if (newPassword != v_password) {
-                      return 'Passwords are not the same'; // Changed error message
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-              ]),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView.builder(
+          itemCount: skills.length,
+          itemBuilder:  (context, index){
+            return Skill(skill: skills[index]);
+          }
         ),
       ),
       bottomSheet: Container(
           width: screenWidth,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          height: 175,
           decoration: BoxDecoration(
               color: Colors.white,
               boxShadow: [
@@ -256,21 +49,85 @@ class _SkillsState extends State<Skills> {
                 )
               ]
           ),
-          height: 75,
-          child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              child: MaterialButton(
-                color: Colors.blue,
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0), // Adjust the border radius
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Kỹ năng"),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: "Nhập kỹ năng của bạn"
                 ),
-                child: Text("Xác nhận",
-                  style: TextStyle(color: Theme.of(context).colorScheme.background),
-                ),
-              )
+              ),
+              SizedBox(height: 10,),
+              Container(
+                  width: screenWidth,
+                  child: MaterialButton(
+                    color: Colors.blue,
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // Adjust the border radius
+                    ),
+                    child: Text("Thêm kỹ năng",
+                      style: TextStyle(color: Theme.of(context).colorScheme.background),
+                    ),
+                  )
+              ),
+            ],
           )
       ),
     );
   }
 }
+
+class Skill extends StatefulWidget {
+  const Skill({required this.skill, super.key});
+  final String skill;
+  @override
+  State<Skill> createState() => _SkillState();
+}
+
+class _SkillState extends State<Skill> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.white, // Set the background color to white
+        borderRadius: BorderRadius.circular(15), // Set the border radius
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3), // Shadow color with some transparency
+            blurRadius: 10, // Blur effect
+            spreadRadius: 1, // Spread effect
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(widget.skill),
+        trailing: Transform.translate(
+          offset: Offset(0, -5),
+          child: GestureDetector(
+            onTap: (){},
+            child: Text("x",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 32,
+                fontWeight: FontWeight.w300
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+  }
+}
+
