244bbf95c1844c6a14503e6f65c5656c18957647
Luong Van Tuan Kiet <142136249+TKauseb2103@users.noreply.github.com>
2024-05-22 16:02:49 +0700

    update all

--- lib/api/api_repository.dart
+++ lib/api/api_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import '../controller/token_manager.dart';
+
 class ApiRepository {
   static const _url = 'https://dever-dashboard-api.onrender.com/api/v1/';
 
@@ -22,9 +24,9 @@ class ApiRepository {
   }
 
   static Future<Map<String, dynamic>> get(
-      {required String path,Map<String, dynamic> options = const {}}) async {
+      {required String path, Map<String, dynamic> options = const {}}) async {
     final response = await http.get(
-      Uri.parse(_url+path),
+      Uri.parse(_url + path),
     );
     final responseData = jsonDecode(response.body);
     return responseData;
@@ -45,6 +47,7 @@ class ApiRepository {
     final responseData = jsonDecode(response.body);
     return responseData;
   }
+
   static Future<Map<String, dynamic>> put(
       {required String path,
       Map<String, dynamic> options = const {},
@@ -64,5 +67,33 @@ class ApiRepository {
   static Future<Map<String, dynamic>> test() async {
     final response = await get(path: "");
     return response;
+  }
+
+  static Future<void> loadAllAPIs() async {
+    final token = await TokenManager().getToken();
+
+    final response1 = await ApiRepository.get(path: 'loadData1', options: {
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response1['status'] == 'success') {
+      final data1 = response1['data'];
+      print('Data 1: $data1');
+
+      final response2 = await ApiRepository.get(path: 'loadData2', options: {
+        'Authorization': 'Bearer $token',
+      });
+
+      if (response2['status'] == 'success') {
+        final data2 = response2['data'];
+        print('Data 2: $data2');
+
+        // Load thêm các API khác nếu cần
+      } else {
+        print('Failed to load data 2: ${response2['message']}');
+      }
+    } else {
+      print('Failed to load data 1: ${response1['message']}');
+    }
   }
 }
